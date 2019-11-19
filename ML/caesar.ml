d4f55c07b707a5a413930e13faed235f1723cf11
barbara stall <barbara.stall1@marist.edu>
2019-11-19 14:03:59 -0500

    Finished debugging Caesar cipher.

--- ML/caesar.ml
+++ ML/caesar.ml
@@ -1,27 +1,23 @@
-# Barbara Stall
-# Functional Programming
-# ML Caesar cipher
-fun shiftChar(char, numShift) = let 
-    val charValue = Char.ord(Char.toUpper(char))
-    in
-    if charValue >= 65 andalso charValue <= 90 then
-        char.chr((charValue -65 + numShift)mod(26) + 65)
-    else char.chr(charValue)
-    end;
-#encrypt
-fun encrypt(string, numShift) = (
-    String.implode(map(fn char => shiftChar(char, numShift))(String.explode(string)))
-);
-#decrypt
-fun decrypt(string, numShift) = (encrypt(string, numShift * ~1));
-#solve
-fun solve(string, maxShift) = (
+fun shiftChar (char,numShift) = (let 
+    val charValue = Char.ord(Char.toUpper(char)) in
+    if (charValue >= 65 andalso charValue <= 90) then
+        (Char.chr((charValue - 65 + numShift) mod(26) + 65))
+    else (Char.chr(charValue))
+    end);
+fun encrypt (string, numShift) = (
+    String.implode(map(fn char => shiftChar(char, numShift))(String.explode(string))))
+;
+fun decrypt (string, numShift) = (encrypt(string, numShift * ~1))
+;
+fun solve (string, maxShift) = (
     print("Caesar " ^ Int.toString(maxShift) ^ ": " ^ encrypt(string, maxShift) ^ "\n");
     if maxShift > 0
         then solve(string, maxShift - 1)
-    else NONE
-);
-#run program
+    else NONE)
+;
 val testStr = "babs test string";
+val encryptedStr = "gfgx yjxy xywnsl";
 print("Original: " ^ testStr ^ "\n");
 print("Encrypted: " ^ encrypt(testStr, 5) ^ "\n");
+print("Decrypted: " ^ decrypt(encryptedStr, 5) ^ "\n");
+solve("DOG", 26);
